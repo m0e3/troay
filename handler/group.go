6988c44d1aa2206e1971959dc56a966510e4062a
m0e3 <m0e3@mail.com>
2022-04-23 06:03:40 -0400

    template of group add

--- handler/group.go
+++ handler/group.go
@@ -1,11 +1,20 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GroupIndex(c *gin.Context) {
 	render(c, "group.html", gin.H{"title": "分组列表"})
 }
-func GroupAdd(c *gin.Context)  {}
+func GroupAdd(c *gin.Context) {
+	if c.Request.Method == http.MethodGet {
+		render(c, "group-add.html", gin.H{"title": "添加分组"})
+		return
+	}
+}
 func GroupEdit(c *gin.Context) {}
 func GroupDel(c *gin.Context)  {}
 func GroupSub(c *gin.Context)  {}
